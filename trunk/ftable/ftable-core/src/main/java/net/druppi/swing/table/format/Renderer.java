28442ff05c96b374601958b7e14cc4100ce02dc6
osechet <osechet@97b2a1c4-6b42-e989-9b9d-8467f60225fd>
2010-08-13 10:35:57 +0000

    Fixed a rendering bug with icons

--- trunk/ftable/ftable-core/src/main/java/net/druppi/swing/table/format/Renderer.java
+++ trunk/ftable/ftable-core/src/main/java/net/druppi/swing/table/format/Renderer.java
@@ -7,12 +7,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
@@ -114,7 +114,7 @@ class Renderer implements TableCellRenderer, Serializable {
                 break;
             }
 
-            label.setText(format.getFormatter().getFormat().format(value));
+            label.setText(format.getFormatter().getFormat().format(label.getText()));
         }
 
         return component;
